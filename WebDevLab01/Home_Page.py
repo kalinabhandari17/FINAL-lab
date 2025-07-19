922e9e7fd0a564620c9ca8a1ba9643a12c5606a0
kalinabhandari17 <kalinabhandari17@gmail.com>
2025-07-18 22:57:05 -0400

    Add files via upload

---
+++ WebDevLab01/Home_Page.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def caption():
+    st.header("***CS 1301 - Kalina Bhandari***")
+    st.write("Welcome to my Lab for CS1301! You can navigate between pages using the sidebar to the left! The following pages are:")
+    st.write("***1. Eric Cartman's Portfolio:*** A portfolio page that showcases the life of Eric Cartman")
+    st.write("***2. Eric's Show Appearances:*** A data page where you can select the parameters!")
+
+
+caption()    
+    
---
+++ WebDevLab01/info.py
@@ -0,0 +1,75 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "images/profile.jpeg"
+about_me = "I'm Eric Cartman. I live in Fairplay, CO and I love to eat fried chicken." 
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/kalina-bhandari-4ba4ab291/"
+my_github_url = "https://github.com/kalinabhandari17"
+my_email_address = "kalinabhandari17.com"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Eating Food',
+    'Institution': 'South Park University',
+    'Location': 'Fairplay, CO',
+    'Graduation Date': 'Never',
+    'GPA': '1.2'
+}
+course_data = {
+    "code":["EAT 1301", "RB 1331", "HIST 1554", "LYS 2050"], 
+    "names":["Intro to Eating", "Ragebaiting 101", "History of Tyrants", "Advanced Lying & Improvisation"], 
+    "semester_taken":["2nd", "3rd", "2nd", "2nd"],
+    "skills":["How to eat 5 tenders in 30 seconds", "Making people mad with few words", "Learning all the evil dictators", "Learning to manipulate and lie"],
+    }
+experience_data = {
+    "Chief Chaos Strategist": (["- Devises manipulative, self-serving plans",
+                                                                          "- create maximum disorder while avoiding all responsibility", "- overall menace"],"images/eric_mean.jpeg"),
+    "Director of Questionable Ethics":(["- Oversees operations that bend (or break) moral guidelines for personal gain",
+                                                           "- usually while blaming others when things go wrong"],"images/eric_evil_two.jpeg"),
+    "Fried Chicken Muncher":(["- Can eat a bucket of KFC is under a minute"],"images/ericchicken.jpeg")
+
+}
+
+projects_data = {
+    "Ultimate Beanie": "Designed the most snug fitting beanie Colorado has ever seen",
+}
+
+programming_data = {
+    "Python": 12,
+    "Java": 50,
+    "C": 14,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "Java": "☕",
+    "C": "🔍",
+}
+spoken_icons = {"French": "🇫🇷",
+    "English": "🇬🇧",
+    "Spanish":"🇪🇸"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "French": "Not Fluent",
+    "Spanish": "Minimal",
+}
+leadership_data = {
+    "Leader of the Freind Group": (["- Self proclaimed leader of Stan, Kenny, and Kyle"],"images/eric_friends.jpeg"),
+
+}
+activity_data = {
+    "Yelling at MOM": ["- Yells at her just for the better-ment of the community"]
+}
